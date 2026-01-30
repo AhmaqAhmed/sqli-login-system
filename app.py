81743a60212e3a2c2f2befb191df454f762832a5
AhmaqAhmed <ahmed.contact103@gmail.com>
2026-01-30 18:20:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,11 @@ def login(usr, pswd):
         print("DB ERROR:", e)
         return False
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     msg = None
+    redirect_url = None
 
     if request.method == "POST":
         usr = request.form.get("username")
@@ -38,24 +40,17 @@ def index():
 
         if login(usr, pswd):
             msg = ("Login successful", "success")
+            redirect_url = "/sec"
         else:
             msg = ("Login failed", "error")
 
-    return render_template("login.html", msg=msg)
+    return render_template("login.html", msg=msg, redirect_url=redirect_url)
+
+
+@app.route("/sec")
+def sec():
+    return render_template("sec.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-"""
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template("login.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
-"""
